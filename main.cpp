292787cc67f827d2aec04aa2444889fc5ca85de7
repyanscky <real.repyanscky@icloud.com>
2022-06-16 16:06:45 +0300

    Final commit

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main()
 {
     std::string wantedFile;
     std::cout << "<File Finder>\t\t(Search Is Case Sensitive!)" << std::endl;
-    std::cout << "Enter a File Name\t(Without File Type And Spaces!\tExample: GLSearchFile) > ";
-    std::cin >> wantedFile;
+    std::cout << "Enter a File Name\t(Without File Type!\tExample: 'file1' or 'file 23') > ";
+    std::getline(std::cin, wantedFile);
     multisearch(wantedFile);
     return 0;
 }
